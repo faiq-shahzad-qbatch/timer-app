52017b38d4e5975ed0ca26662bf91500955849a4
Faiq Shahzad <faiq.shahzad@qbatch.com>
2023-08-21 19:40:07 +0500

    today's task

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import TimerApp from "./TimerApp";
+// import TimerApp from "./TimerApp";
 import reportWebVitals from "./reportWebVitals";
 
-// import CardDisplayerApp from "./CardDisplayerApp";
+import CardDisplayerApp from "./CardDisplayerApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <>
-    {/* <CardDisplayerApp /> */}
-    <TimerApp />
+    <CardDisplayerApp />
+    {/* <TimerApp /> */}
   </>,
   // </React.StrictMode>,
 );
